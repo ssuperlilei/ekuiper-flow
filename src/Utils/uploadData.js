e2ebe2b0cef24ec6c523666462160f5af713047d
ysfscream <894402575bt@gmail.com>
2022-05-07 23:38:04 +0800

    feat(upload_data): add trans nodes data function

--- src/Utils/uploadData.js
+++ src/Utils/uploadData.js
@@ -1,14 +1,40 @@
 const uploadNodes = (nodes, edges) => {
-  console.log(nodes, edges);
+  // nodes
+  const _nodes = {};
+  const tranType = ({ name, nodeType }) => {
+    let _type = nodeType;
+    let _nodeType = nodeType;
+    if (nodeType === 'function') {
+      _type = 'operator';
+    } else {
+      _nodeType = name;
+    }
+    return {
+      _type,
+      _nodeType,
+    };
+  };
+  nodes.forEach((node) => {
+    const { data, ...otherInfo } = node;
+    const { name, configs } = data;
+    const { _type, _nodeType } = tranType(data);
+    _nodes[name] = {
+      type: _type,
+      nodeType: _nodeType,
+      prop: {
+        ...configs,
+        meta: otherInfo,
+      },
+    };
+  });
+  // edges
+  const _topo = {};
   const data = {
     id: 'rule1',
     name: '规则ui',
     graph: {
-      nodes: {},
-      topo: {
-        source: [],
-        edges: [],
-      },
+      nodes: _nodes,
+      topo: _topo,
     },
   };
 };
